0b34cecf7998a2df4beece334871301b7559b738
Jesse Donat <donatj@gmail.com>
2013-08-22 12:03:43 -0500

    initial work on testcase

---
+++ src/Phrisby/Exceptions/ExpectFailedException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Phrisby\Exceptions;
+
+class ExpectFailedException extends \Exception {
+
+}
--- src/Phrisby/TestCase.php
+++ src/Phrisby/TestCase.php
@@ -2,9 +2,33 @@
 
 namespace Phrisby;
 
+use Phrisby\Exceptions\ExpectFailedException;
+
 class TestCase {
 
+	private $response;
+	private $header_sets;
 	public function __construct( $response, array $response_headers = null ) {
+		$this->response    = $response;
+		$this->header_sets = $response_headers;
+	}
+
+	/**
+	 * @param int  $status
+	 * @param null $hop
+	 * @return $this
+	 * @throws Exceptions\ExpectFailedException
+	 */
+	public function expectStatus( $status = 200, $hop = null ) {
+		$headers = $this->hopHeaders($hop);
+
+		preg_match('|HTTP/\d\.\d\s+(\d+)\s+.*|', $headers[0], $match);
+
+		if( $match[1] != $status ) {
+			$this->exceptions[] = new ExpectFailedException( 'Expected status ' . var_dump($match[1]) . ' to match ' . $status );
+		}
+
+		return $this;
 
 	}
 
