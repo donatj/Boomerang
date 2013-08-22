0e5af1af592eead119d33c7ea9a25e82ef2d6774
Jesse Donat <donatj@gmail.com>
2013-08-22 15:38:43 -0500

    ExpectFailedException extended to pass the test at hand

--- src/Phrisby/Exceptions/ExpectFailedException.php
+++ src/Phrisby/Exceptions/ExpectFailedException.php
@@ -2,6 +2,22 @@
 
 namespace Phrisby\Exceptions;
 
+use Phrisby\TestCase;
+
 class ExpectFailedException extends \Exception {
+
+	private $test;
+
+	public function __construct($message, TestCase $test, $code = 0) {
+		parent::__construct($message, $code);
+		$this->test = $test;
+	}
+
+
+	public function getTest() {
+		return $this->test;
+	}
+
+
 
 }
