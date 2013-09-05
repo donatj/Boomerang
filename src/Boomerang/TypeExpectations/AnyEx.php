1c73d9b7956de94b6ebddd660ec7d0bf6f29759f
Jesse Donat <donatj@gmail.com>
2013-09-05 17:10:27 -0500

    todo added to AnyEx noting issues

--- src/Boomerang/TypeExpectations/AnyEx.php
+++ src/Boomerang/TypeExpectations/AnyEx.php
@@ -5,6 +5,9 @@ namespace Boomerang\TypeExpectations;
 class AnyEx extends AllEx {
 
 	function match($data) {
+
+		// @todo: __validate should do something other than throw the exceptions into an array, because this can't currently work
+
 		foreach($this->structures as $struct) {
 			if( $this->__validate($data, $struct) ) {
 				return true;
