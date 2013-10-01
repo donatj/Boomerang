b0ec158463775e5514ab64fc3c6f6e47855c1312
Jesse Donat <donatj@gmail.com>
2013-10-01 18:08:01 -0500

    NullExTest added

---
+++ test/TypeExpectations/NullExTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Boomerang\Test\TypeExpectations;
+
+use Boomerang\TypeExpectations\NullEx;
+
+class NullExTest extends \PHPUnit_Framework_TestCase {
+
+	public function testBasicMatching() {
+
+		$x = new NullEx();
+
+		$this->assertEquals(false, $x->match(false));
+		$this->assertEquals(false, $x->match(true));
+		$this->assertEquals(true, $x->match(null));
+		$this->assertEquals(false, $x->match('abc'));
+		$this->assertEquals(false, $x->match('23'));
+		$this->assertEquals(false, $x->match(23));
+		$this->assertEquals(false, $x->match('23.5'));
+		$this->assertEquals(false, $x->match(23.5));
+		$this->assertEquals(false, $x->match(''));
+		$this->assertEquals(false, $x->match(' '));
+		$this->assertEquals(false, $x->match('0'));
+		$this->assertEquals(false, $x->match(0));
+
+	}
+
+}
