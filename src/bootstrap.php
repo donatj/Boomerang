28116099a9122e5c27254f834a1ed5376cbb1b0a
Jesse Donat <donatj@gmail.com>
2013-10-07 16:32:20 -0500

    Update bootstrap to prevent an E_STRICT warning caused by PHPUnit making my travis-ci builds fail

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -9,6 +9,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
+error_reporting( E_ALL & ~E_STRICT );
 
 date_default_timezone_set( @date_default_timezone_get() ); //feels hackish
 
