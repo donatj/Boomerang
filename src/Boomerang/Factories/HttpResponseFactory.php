eec968b29bf80712dac088d7cc078181e306b573
Jesse Donat <donatj@gmail.com>
2013-12-02 11:53:39 -0600

    Wrong HttpResponse object in HttpResponseFactory

--- src/Boomerang/Factories/HttpResponseFactory.php
+++ src/Boomerang/Factories/HttpResponseFactory.php
@@ -3,11 +3,12 @@
 namespace Boomerang\Factories;
 
 use Boomerang\HttpRequest;
+use Boomerang\HttpResponse;
 
 class HttpResponseFactory {
 
 	function newInstance( $body, $headers, HttpRequest $request = null ) {
-		return new \HttpResponse($body, $headers, $request);
+		return new HttpResponse($body, $headers, $request);
 	}
 
 }
