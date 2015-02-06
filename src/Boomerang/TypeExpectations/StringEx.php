e300470d62afdcac90351bad9e7ef44eff5b02df
Jesse Donat <donatj@gmail.com>
2015-02-06 17:15:52 -0600

    StringEx now has a minLength and maxLength property

--- src/Boomerang/TypeExpectations/StringEx.php
+++ src/Boomerang/TypeExpectations/StringEx.php
@@ -13,12 +13,32 @@ use Boomerang\Interfaces\TypeExpectationInterface;
  */
 class StringEx implements TypeExpectationInterface {
 
+	protected $minLength;
+	protected $maxLength;
+
+	/**
+	 * @param null|int $minLength Optional minimum length in bytes of a valid value
+	 * @param null|int $maxLength Optional maximum length in bytes of a valid value
+	 */
+	public function __construct( $minLength = null, $maxLength = null ) {
+		$this->minLength = $minLength;
+		$this->maxLength = $maxLength;
+	}
+
 	public function match( $data ) {
-		return is_string($data);
+		return is_string($data)
+			   && $this->rangeValidation($data);
+	}
+
+	protected function rangeValidation( $data ) {
+		$len = strlen($data);
+
+		return ($len >= $this->minLength || $this->minLength === null)
+			   && ($len <= $this->maxLength || $this->maxLength === null);
 	}
 
 	public function getMatchingTypeName() {
-		return 'string';
+		return sprintf('string{%s,%s}', intval($this->minLength), is_null($this->maxLength) ? '∞' : $this->maxLength);
 	}
 
 }
