ec0ee65d210c0b9aabf9380fb935388d0266db7e
Jesse Donat <donatj@gmail.com>
2026-08-05 06:27:27 -0500

    Restore UserInterface defensive guards

--- src/Boomerang/Runner/UserInterface.php
+++ src/Boomerang/Runner/UserInterface.php
@@ -3,12 +3,14 @@
 namespace Boomerang\Runner;
 
 use Boomerang\Boomerang;
+use Boomerang\Exceptions\CliRuntimeException;
 use Boomerang\ExpectationResults\FailingExpectationResult;
 use Boomerang\ExpectationResults\FailingResult;
 use Boomerang\ExpectationResults\InfoResult;
 use Boomerang\ExpectationResults\MutedExpectationResult;
 use Boomerang\ExpectationResults\PassingExpectationResult;
 use Boomerang\ExpectationResults\PassingResult;
+use Boomerang\Interfaces\ExpectationResultInterface;
 use Boomerang\Interfaces\ResponseValidatorInterface;
 use Boomerang\Interfaces\ValidatorInterface;
 use CLI\Output;
@@ -59,25 +61,29 @@ EOT;
 	public function updateExpectationDisplay( $file, $validators, $verbose = false ) {
 
 		foreach( $validators as $validator ) {
-			$dot = false;
+			if( $validator instanceof ValidatorInterface ) {
+				$dot = false;
 
-			foreach( $validator->getExpectationResults() as $expectationResult ) {
-				if( $expectationResult instanceof FailingResult ) {
-					$dot = Style::red("F");
-				} elseif( $expectationResult instanceof InfoResult ) {
-					$dot = Style::normal("I");
-				} elseif( $expectationResult instanceof MutedExpectationResult ) {
-					$dot = false;
-				} elseif( !$expectationResult instanceof PassingResult ) {
-					$dot = Style::red("?");
-				}
+				foreach( $validator->getExpectationResults() as $expectationResult ) {
+					if( $expectationResult instanceof FailingResult ) {
+						$dot = Style::red("F");
+					} elseif( $expectationResult instanceof InfoResult ) {
+						$dot = Style::normal("I");
+					} elseif( $expectationResult instanceof MutedExpectationResult ) {
+						$dot = false;
+					} elseif( !$expectationResult instanceof PassingResult ) {
+						$dot = Style::red("?");
+					}
 
-				if( $dot ) {
-					break;
+					if( $dot ) {
+						break;
+					}
 				}
-			}
-			if( !$verbose ) {
-				Output::string($dot ?: Style::green("."));
+				if( !$verbose ) {
+					Output::string($dot ?: Style::green("."));
+				}
+			} else {
+				throw new CliRuntimeException("Error: Unexpected ValidatorInterface");
 			}
 		}
 
@@ -93,73 +99,79 @@ EOT;
 					continue;
 				}
 
-				$notPassing = !($expectationResult instanceof PassingResult);
-				if( $notPassing || $verbose ) {
+				if( $expectationResult instanceof ExpectationResultInterface ) {
+					$notPassing = !($expectationResult instanceof PassingResult);
+					if( $notPassing || $verbose ) {
 
-					if( !$initialWhitespace ) {
-						Output::string(PHP_EOL);
-						$initialWhitespace = true;
-					}
-
-					if( $validator instanceof ResponseValidatorInterface ) {
-						$endpoint = $validator->getResponse()->getRequest()->getEndpoint();
-					} else {
-						$endpoint = false;
-					}
-
-					if( $notPassing || $verbose > 1 ) {
-						Output::string(PHP_EOL . Style::light_gray("# " . str_repeat('-', 25)) . PHP_EOL);
-					}
-
-					if( !$fileDisplayed ) {
-						Output::string(Style::red($file) . PHP_EOL);
-						$fileDisplayed = true;
-					}
-
-					if( $endpoint && $endpoint != $lastEndpoint ) {
-						Output::string("[ " . Style::blue($endpoint, 'underline') . " ]");
-						if( $verbose ) {
-							/**
-							 * @var $validator ResponseValidatorInterface
-							 */
-							Output::string('( ' . $validator->getResponse()->getRequest()->getLastRequestTime() . 's )');
+						if( !$initialWhitespace ) {
+							Output::string(PHP_EOL);
+							$initialWhitespace = true;
 						}
-						Output::string(PHP_EOL);
-					}
 
-					if( $notPassing || $verbose > 1 ) {
-						Output::string(PHP_EOL . $expectationResult->getMessage() . PHP_EOL . PHP_EOL);
+						if( $validator instanceof ResponseValidatorInterface ) {
+							$endpoint = $validator->getResponse()->getRequest()->getEndpoint();
+						} else {
+							$endpoint = false;
+						}
 
-						if( $expectationResult instanceof FailingExpectationResult ) {
-							$actual   = $expectationResult->getActual();
-							$expected = $expectationResult->getExpected();
+						if( $notPassing || $verbose > 1 ) {
+							Output::string(PHP_EOL . Style::light_gray("# " . str_repeat('-', 25)) . PHP_EOL);
+						}
 
-							if( $expectationResult->getActual() !== null ) {
-								Output::string("Actual: " . PHP_EOL);
-								Output::string(var_export($actual, true));
-								Output::string(PHP_EOL);
+						if( !$fileDisplayed ) {
+							Output::string(Style::red($file) . PHP_EOL);
+							$fileDisplayed = true;
+						}
+
+						if( $endpoint && $endpoint != $lastEndpoint ) {
+							Output::string("[ " . Style::blue($endpoint, 'underline') . " ]");
+							if( $verbose ) {
+								/**
+								 * @var $validator ResponseValidatorInterface
+								 */
+								Output::string('( ' . $validator->getResponse()->getRequest()->getLastRequestTime() . 's )');
 							}
+							Output::string(PHP_EOL);
+						}
 
-							if( $expected !== null ) {
-								Output::string(PHP_EOL);
-								Output::string("Expected: " . PHP_EOL);
-								Output::string(Style::red(var_export($expected, true)));
-								Output::string(PHP_EOL);
-							}
-						} elseif( $expectationResult instanceof PassingExpectationResult ) {
-							$actual = $expectationResult->getActual();
+						if( $notPassing || $verbose > 1 ) {
+							Output::string(PHP_EOL . $expectationResult->getMessage() . PHP_EOL . PHP_EOL);
 
-							if( $verbose > 2 ) {
+							if( $expectationResult instanceof FailingExpectationResult ) {
+								$actual   = $expectationResult->getActual();
+								$expected = $expectationResult->getExpected();
+
 								if( $expectationResult->getActual() !== null ) {
-									Output::string("Actual as Expected: " . PHP_EOL);
-									Output::string(Style::green(var_export($actual, true)));
+									Output::string("Actual: " . PHP_EOL);
+									Output::string(var_export($actual, true));
 									Output::string(PHP_EOL);
+								}
+
+								if( $expected !== null ) {
+									Output::string(PHP_EOL);
+									Output::string("Expected: " . PHP_EOL);
+									Output::string(Style::red(var_export($expected, true)));
+									Output::string(PHP_EOL);
+								}
+							} elseif( $expectationResult instanceof PassingExpectationResult ) {
+								$actual = $expectationResult->getActual();
+
+								if( $verbose > 2 ) {
+									if( $expectationResult->getActual() !== null ) {
+										Output::string("Actual as Expected: " . PHP_EOL);
+										Output::string(Style::green(var_export($actual, true)));
+										Output::string(PHP_EOL);
+									}
 								}
 							}
 						}
-					}
 
-					$lastEndpoint = $endpoint;
+						$lastEndpoint = $endpoint;
+					}
+				} elseif( is_string($expectationResult) ) {
+					$this->outputMsg('MSG: ' . $expectationResult);
+				} else {
+					$this->outputMsg("Error: Unexpected Expectation:" . var_export($expectationResult, true));
 				}
 			}
 		}
